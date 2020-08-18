171979a478d8ab02005871ccafa35f04344cac4c
Ina Yoon <itsina96@gmail.com>
2020-08-18 14:53:50 +0100

    Add typeface in global style file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React from 'react';
+import GlobalStyle from './globalstyle/GlobalStyle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalStyle>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </GlobalStyle>
   );
 }
 
---
+++ src/globalstyle/GlobalStyle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+require('typeface-open-sans');
+
+const GlobalStyle = styled.div`
+  font-family: open sans;
+`;
+
+export default GlobalStyle;
