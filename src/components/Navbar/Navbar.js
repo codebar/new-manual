b83f471a079fdc416b4a2413330779d5fd645a28
Lizzy-j <lizzyhj@hotmail.com>
2020-08-18 18:20:50 +0100

    navbar styling test Relates #16
    Co-authored-by: itsina96 <itsina96@gmail.com>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from 'react-router-dom';
 import logo from "../../assets/image/website-logo.png";
-
+import Logo from "./Navbar.style"
 const Navbar = () => {
     return (
       <>
         <Link to="/">
-          <img src={logo} alt="codebar-logo" />
+          <Logo src={logo} alt="codebar-logo" />
         </Link>
         <Link to="/about">About</Link>
         <Link to="/sponsors">Sponsors</Link>
--- src/components/Navbar/Navbar.style.js
+++ src/components/Navbar/Navbar.style.js
@@ -1,3 +1,9 @@
 import styled, { keyframes } from 'styled-components';
 
 
+const Logo = styled.img`
+width:2rem;
+background-color: red;
+`
+
+export default Logo;
