fcde7d72a1adf5ff8cd2489621516704556fa19f
Lizzy-j <lizzyhj@hotmail.com>
2020-08-19 16:33:03 +0100

    add images for about page and intro text plus basic styling

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,14 @@
 import React from "react";
+import {Heading, Subheading} from './About.style'
 
 const About = () => {
     return (
-        <h1>This is about page</h1>
+        <>
+        <Heading>We are codebar.</Heading>
+        <Subheading>We are a registered charity that has been running weekly coding workshops since October 2013, with London as its first chapter. </Subheading>
+        
+        <Subheading>Since then we have expanded to running workshops in 19 different cities from Brighton to Manchester and Edinburgh to New York, and Berlin, Sydney, and the list goes on.</Subheading>
+        </>
     )
 }
 
---
+++ src/components/About/About.style.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import { GlobalStyle } from '../../globalstyle/GlobalStyle';
+
+
+const darkGray = '#808080';
+
+
+const Heading = styled.h1`
+    font-family: ${GlobalStyle};
+`
+
+const Subheading = styled.p`
+    font-family: ${GlobalStyle};
+    color: ${darkGray};
+    font-weight: lighter;
+    font-size: 1.5rem
+`
+
+
+
+export {Heading, Subheading};
