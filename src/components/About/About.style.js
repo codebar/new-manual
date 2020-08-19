a79969496b4d5d9e28ace8f56d156da93948c7b5
Lizzy-j <lizzyhj@hotmail.com>
2020-08-19 19:41:50 +0100

    add styling to about page

--- src/components/About/About.style.js
+++ src/components/About/About.style.js
@@ -8,13 +8,21 @@ const black = '#000000';
 
 const Heading = styled.h1`
     font-family: ${GlobalStyle};
+    margin-left: 4rem;
+    line-height: 1.5;
 `
 
 const Subheading = styled.p`
     font-family: ${GlobalStyle};
     color: ${darkGray};
     font-weight: lighter;
-    font-size: 1.5rem
+    font-size: 1.5rem;
+    margin-left: 4rem;
+    line-height: 1.5;
+`
+
+const Introtext  = styled.div`
+    max-width: 80%;
 `
 
 const p = styled.p`
@@ -23,6 +31,36 @@ const p = styled.p`
     font-size: 1.4rem;
 `
 
+const Img = styled.img`
+    height: 491px;
+    width: 675px;
+    border-radius: 40%;
+`
+
+const Contentwrapper = styled.div`
+    display: grid;
+    grid-auto-flow: column;
+    margin: 6rem 2rem 3rem 4rem;
+    grid-template-columns: 800px 1000px;
+    grid-column-gap: 10px;
+    align-items: center;
+`
+
+const Contenttext = styled.p`
+    font-size: 1.4rem;
+    text-align: right;
+    max-width: 400px;
+    font-weight: lighter;
+    line-height: 1.7;
+`
 
 
-export {Heading, Subheading};
+const ContenttextAlt = styled.p`
+    font-size: 1.4rem;
+    max-width: 600px;
+    font-weight: lighter;
+    line-height: 1.7;
+    text-align: right;
+`
+
+export {Heading, Subheading, Img, Introtext, p, Contentwrapper, Contenttext, ContenttextAlt };
