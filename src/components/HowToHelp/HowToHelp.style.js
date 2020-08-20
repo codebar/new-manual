7a8b8767d631117766e0efc9d2e50382c6e00259
Ina Yoon <itsina96@gmail.com>
2020-08-20 15:32:34 +0100

    Combine button components into HowToHelp component

--- src/components/HowToHelp/HowToHelp.style.js
+++ src/components/HowToHelp/HowToHelp.style.js
@@ -1,22 +1,41 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const lightGray = '#F4F4F4';
+const darkGrayAlt = '#3D4A54';
+// const middleGray = "#586776";
+
+const SponsorsBtn = styled(Link)`
+  background-color: ${darkGrayAlt};
+  padding: 1rem 2rem;
+  color: white;
+  text-decoration: none;
+  margin: 3rem;
+`;
+const OrganisersBtn = styled(Link)`
+  background-color: ${darkGrayAlt};
+  padding: 1rem 2rem;
+  color: white;
+  text-decoration: none;
+  margin: 2rem;
+`;
 
 const HowtoHelpWrapper = styled.div`
   background-color: ${lightGray};
-  height: 40vh;
+  height: 20rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
 
-const HowToHelpSubtitle = styled.h2``;
-const SponsorsBtn = styled.a``;
-const OrganisersBtn = styled.a``;
-const HowToHelpBtnWrapper = styled.div`
-  display: ;
+const HowToHelpSubtitle = styled.h2`
+  font-weight: lighter;
+  font-size: 2.5rem;
+  padding-bottom: 1rem;
 `;
+
+const HowToHelpBtnWrapper = styled.div``;
 
 export {
   HowtoHelpWrapper,
