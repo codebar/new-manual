5656658035e8fbc49cb7cee85168cd5715e9276a
Lizzy-j <lizzyhj@hotmail.com>
2020-08-20 16:11:32 +0100

    basic media query for anything else section

--- src/components/Organisers/Organisers.style.js
+++ src/components/Organisers/Organisers.style.js
@@ -13,19 +13,32 @@ const IntroText = styled.div`
     color: ${black};
     margin: 5rem 5rem 4rem 5rem;
     max-width: 70%;
+        @media screen and (max-width:400px){
+        font-size: 0.9rem;
+        margin: 1rem 1rem 1rem 1em;
+        max-width: 90%;
+        text-align: justify;
+    }
+
 `
 const Heading = styled.h1`
     font-weight: 600;
     text-align: center;
     margin-bottom: 4rem;
         @media screen and (max-width:400px){
-            font-size: 2rem;
+            font-size: 1.5rem;
+            margin-bottom: 1rem;
         }
 `
 
 const Subheading = styled.h2`
     font-weight: 550;
     color: ${black};
+        @media screen and (max-width:400px){
+        font-size: 1rem;
+        margin-top: 1rem;
+        text-align: center;
+    }
 `
 const SubheadingAlt = styled.h2`
     font-weight: 550;
@@ -34,11 +47,20 @@ const SubheadingAlt = styled.h2`
 const AnythingElseWrapper = styled.div`
     background-color: ${lightGrey};
     width: 100%;
-    padding: 4rem 6rem 4rem 6rem;
+    // padding: 4rem 6rem 4rem 6rem;
+        @media screen and (max-width:400px){
+            font-size: 0.9rem;
+            padding: 1rem 1rem 1rem 1em;
+            max-width: 90%;
+            text-align: justify;
+        }
 `
 
 const AnythingElseText = styled.p`
     max-width: 80%;
+    @media screen and (max-width: 400px){
+        margin: 0 auto;
+    }
 `
 const EventWrapper = styled.div`
     color: ${black};
