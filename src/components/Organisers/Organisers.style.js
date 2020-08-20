d8fddd096069fd90a1f6c324b04d36bf7153f861
Lizzy-j <lizzyhj@hotmail.com>
2020-08-20 12:14:32 +0100

    add text and basic styling

---
+++ src/components/Organisers/Organisers.style.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+const black = '#000000';
+
+
+const Wrapper = styled.div`
+    margin: 5rem 5rem 4rem 5rem;
+`
+
+const IntroText = styled.div`
+    color: ${black};
+`
+
+const AnythingElseWrapper = styled.div`
+    color: 'purple';
+`
+const EventWrapper = styled.div`
+    color: 'green';
+`
+
+const OtherInfoWrapper = styled.div`
+    color: purple;
+`
+
+
+export {Wrapper, IntroText, AnythingElseWrapper, EventWrapper, OtherInfoWrapper};
