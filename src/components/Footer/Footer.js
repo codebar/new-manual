d7c07fca1d0c72150a735cb41d4ccccb867e02b8
Ina Yoon <itsina96@gmail.com>
2020-08-20 01:11:15 +0100

    Start working on footer & add svg icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,33 @@
 import React from 'react';
-import { FooterWrapper } from './Footer.style';
-
+import {
+  FooterWrapper,
+  FooterEmail,
+  FooterSocials,
+  FooterText,
+} from './Footer.style';
+import twitter from '../../assets/image/twitter-logo.svg';
+import slack from '../../assets/image/slack-logo.svg';
+import facebook from '../../assets/image/facebook-logo.svg';
+import github from '../../assets/image/github-logo.svg';
 const Footer = () => {
-  return <FooterWrapper></FooterWrapper>;
+  return (
+    <FooterWrapper>
+      <FooterText>
+        <p>send us email at</p>
+        <FooterEmail href="mailto:hello@codebar.io">
+          hello@codebar.io
+        </FooterEmail>
+        <p>Registered UK and Wales charity no. 1187776</p>
+        <p>© 2020 codebar</p>
+      </FooterText>
+      <FooterSocials>
+        <img src={slack} />
+        <img src={twitter} />
+        <img src={facebook} />
+        <img src={github} />
+      </FooterSocials>
+    </FooterWrapper>
+  );
 };
 
 export default Footer;
--- src/components/Footer/Footer.style.js
+++ src/components/Footer/Footer.style.js
@@ -3,6 +3,20 @@ import styled from 'styled-components';
 const FooterWrapper = styled.footer`
   background-color: #3e4954;
   min-height: 30vh;
+  display: flex;
+  color: white;
+  bottom: 0;
 `;
 
-export { FooterWrapper };
+const FooterEmail = styled.a`
+  font-size: 2rem;
+  font-weight: lighter;
+  text-decoration: none;
+  border-bottom: 1px solid white;
+  padding: 0.1rem 0;
+  color: white;
+`;
+
+const FooterText = styled.div``;
+const FooterSocials = styled.div``;
+export { FooterWrapper, FooterEmail, FooterSocials, FooterText };
